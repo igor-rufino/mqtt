0ed2b3ac1ddd8012deb43257003defad27c60396
Igor Rufino Ribeiro <40177904+igor-rufino@users.noreply.github.com>
2022-06-04 15:15:23 -0300

    Cria projeto labmqtt

---
+++ MQTT/labmqtt/labmqtt/src/main/java/br/inatel/sd/labmqtt/client/SensorTemperaturaPublisher.java
@@ -0,0 +1,40 @@
+package br.inatel.sd.labmqtt.client;
+
+import java.util.Random;
+import java.util.UUID;
+
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+
+public class SensorTemperaturaPublisher {
+
+	public static void main(String[] args) throws InterruptedException, MqttException {
+		String publisherId = UUID.randomUUID().toString();
+		IMqttClient publisher = new MqttClient( MyConstants.URI_BROKER, publisherId);
+
+		MqttMessage msg = getTemperaturaSolo();
+		msg.setQos(0);
+		msg.setRetained(true);
+
+		MqttConnectOptions options = new MqttConnectOptions();
+		options.setAutomaticReconnect(true);
+		options.setCleanSession(true);
+		options.setConnectionTimeout(10);
+		publisher.connect(options);
+
+		publisher.publish(MyConstants.TOPIC_1, msg);
+		
+		publisher.disconnect();
+	}
+	
+	private static MqttMessage getTemperaturaSolo() {
+		Random r = new Random();
+		double temperatura = 80 + r.nextDouble() * 20.0;
+		byte[] payload = String.format("T:%04.2f", temperatura).getBytes();
+		return new MqttMessage(payload);
+	}
+}
+
